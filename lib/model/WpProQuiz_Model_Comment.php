24971b7e915308a01d02086ee2913b3c4be8b4a6
VovanOst <vovan.ost@mail.ru>
2018-09-27 17:16:55 +0300

    'initializiation'

---
+++ lib/model/WpProQuiz_Model_Comment.php
@@ -0,0 +1,209 @@
+<?php
+
+class WpProQuiz_Model_Comment extends WpProQuiz_Model_Model
+{
+   /* const FORM_TYPE_TEXT = 0;
+    const FORM_TYPE_TEXTAREA = 1;
+    const FORM_TYPE_NUMBER = 2;
+    const FORM_TYPE_CHECKBOX = 3;
+    const FORM_TYPE_EMAIL = 4;
+    const FORM_TYPE_YES_NO = 5;
+    const FORM_TYPE_DATE = 6;
+    const FORM_TYPE_SELECT = 7;
+    const FORM_TYPE_RADIO = 8;*/
+
+    protected $_commentId = 0;
+    protected $_quizId = 0;
+	protected $_questionId = 0;
+    protected $_statisticRefId=0;
+	protected $_comment='';
+	protected $_createTime = 0;
+	protected $_userName = '';
+   // protected $_fieldname = '';
+   // protected $_type = 0;
+  //  protected $_required = false;
+  //  protected $_sort = 0;
+   // protected $_data = null;
+   // protected $_showInStatistic = false;
+
+    public function setCommentId($_commentId)
+    {
+        $this->_commentId = (int)$_commentId;
+
+        return $this;
+    }
+
+    public function getCommentId()
+    {
+        return $this->_commentId;
+    }
+
+    public function setQuizId($_quizId)
+    {
+        $this->_quizId = (int)$_quizId;
+
+        return $this;
+    }
+
+    public function getQuizId()
+    {
+        return $this->_quizId;
+    }
+
+	public function setQuestionId($_questionId)
+	{
+		$this->_questionId = (int)$_questionId;
+
+		return $this;
+	}
+
+	public function getQuestionId()
+	{
+		return $this->_questionId;
+	}
+
+	public function setStatisticRefId($_statisticRefId)
+	{
+		$this->_statisticRefId = (int)$_statisticRefId;
+
+		return $this;
+	}
+
+	public function getStatisticRefId()
+	{
+		return $this->_statisticRefId;
+	}
+
+
+	public function setComment($_comment)
+	{
+		$this->_comment = (string)$_comment;
+
+		return $this;
+	}
+
+	public function getComment()
+	{
+		return $this->_comment;
+	}
+
+
+	public function setCreateTime($_createTime)
+	{
+		$this->_createTime = (int)$_createTime;
+
+		return $this;
+	}
+
+	public function getCreateTime()
+	{
+		return $this->_createTime;
+	}
+
+	public function setUserName($_userName)
+	{
+		$this->_userName = (string)$_userName;
+
+		return $this;
+	}
+
+	public function getUserName()
+	{
+		return $this->_userName;
+	}
+
+	public function setMinCreateTime($_minCreateTime)
+	{
+		$this->_minCreateTime = (int)$_minCreateTime;
+
+		return $this;
+	}
+
+	public function getMinCreateTime()
+	{
+		return $this->_minCreateTime;
+	}
+
+	public function setMaxCreateTime($_maxCreateTime)
+	{
+		$this->_maxCreateTime = (int)$_maxCreateTime;
+
+		return $this;
+	}
+
+	public function getMaxCreateTime()
+	{
+		return $this->_maxCreateTime;
+	}
+    /*public function setFieldname($_fieldname)
+    {
+        $this->_fieldname = (string)$_fieldname;
+
+        return $this;
+    }*/
+
+  /*  public function getFieldname()
+    {
+        return $this->_fieldname;
+    }
+
+    public function setType($_type)
+    {
+        $this->_type = (int)$_type;
+
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->_type;
+    }
+
+    public function setRequired($_required)
+    {
+        $this->_required = (bool)$_required;
+
+        return $this;
+    }
+
+    public function isRequired()
+    {
+        return $this->_required;
+    }
+
+    public function setSort($_sort)
+    {
+        $this->_sort = (int)$_sort;
+
+        return $this;
+    }
+
+    public function getSort()
+    {
+        return $this->_sort;
+    }*/
+	/*
+	public function setData($_data)
+	{
+		$this->_data = $_data === null ? null : (array)$_data;
+
+		return $this;
+	}
+
+	public function getData()
+	{
+		return $this->_data;
+	}
+
+	public function setShowInStatistic($_showInStatistic)
+	{
+		$this->_showInStatistic = (bool)$_showInStatistic;
+
+		return $this;
+	}
+
+	public function isShowInStatistic()
+	{
+		return $this->_showInStatistic;
+	}*/
+}
---
+++ lib/model/WpProQuiz_Model_CommentMapper.php
@@ -0,0 +1,125 @@
+<?php
+
+class WpProQuiz_Model_CommentMapper extends WpProQuiz_Model_Mapper
+{
+
+   /* public function deleteComment($commentIds, $quizId)
+    {
+        return $this->_wpdb->query(
+            $this->_wpdb->prepare('
+				DELETE FROM ' . $this->_tableForm . '
+				WHERE
+					comment_id IN(' . implode(', ', array_map('intval', (array)$commentIds)) . ') AND quiz_id = %d
+			', $quizId)
+        );
+    }*/
+
+    /**
+     * @param WpProQuiz_Model_Comment[] $comments
+     */
+   /* public function update($comments)
+    {
+        $values = $values2 = array();
+
+        foreach ($comments as $comment) {*/
+            /* @var $form WpProQuiz_Model_Form */
+
+          /*  $data = array(
+                $comment->getFormId(),
+                $comment->getQuizId(),
+                $comment->getFieldname(),
+                $comment->getType(),
+                (int)$comment->isRequired(),
+                $comment->getSort(),
+                (int)$comment->isShowInStatistic()
+            );
+
+            if ($form->getData() === null) {
+                $values[] = '(' . $this->_wpdb->prepare('%d, %d, %s, %d, %d, %d, %d', $data) . ')';
+            } else {
+                $data[] = @json_encode($form->getData());
+                $values2[] = '(' . $this->_wpdb->prepare('%d, %d, %s, %d, %d, %d, %d, %s', $data) . ')';
+            }
+        }
+
+        if (!empty($values)) {
+            $this->_wpdb->query('
+				REPLACE INTO ' . $this->_tableForm . '
+					(form_id, quiz_id, fieldname, type, required, sort, show_in_statistic)
+				VALUES ' . implode(', ', $values) . '
+			');
+        }
+
+        if (!empty($values2)) {
+            $this->_wpdb->query('
+				REPLACE INTO ' . $this->_tableForm . '
+					(form_id, quiz_id, fieldname, type, required, sort, show_in_statistic, data)
+				VALUES ' . implode(', ', $values2) . '
+			');
+        }
+    }*/
+
+    /**
+     * @param $quizId
+     * @return WpProQuiz_Model_Form[]
+     */
+   /* public function fetch($quizId)
+    {
+        $results = $this->_wpdb->get_results(
+            $this->_wpdb->prepare('
+						SELECT * FROM ' . $this->_tableForm . ' WHERE quiz_id = %d ORDER BY sort', $quizId), ARRAY_A);
+        $a = array();
+
+        foreach ($results as $row) {
+            $row['data'] = $row['data'] === null ? null : @json_decode($row['data'], true);
+
+            $a[] = new WpProQuiz_Model_Form($row);
+        }
+
+        return $a;
+    }*/
+	public function fetchAllByRef($statisticRefId)
+	{
+		$a = array();
+
+		$results = $this->_wpdb->get_results(
+			$this->_wpdb->prepare(
+				'SELECT
+							*
+						FROM
+							' . $this->_tableComment . '
+						WHERE
+							statistic_ref_id = %d', $statisticRefId),
+			ARRAY_A);
+
+		foreach ($results as $row) {
+			$a[] = new WpProQuiz_Model_Comment($row);
+		}
+
+		return $a;
+	}
+
+	public function fetchByRefId($refIdUserId, $quizId, $avg = false)
+	{
+		$where = $avg ? 'comment.user_id = %d' : 'comment.statistic_ref_id = %d';
+		$results = $this->_wpdb->get_results(
+			$this->_wpdb->prepare(
+				"SELECT
+					comment.*,
+					comment.create_time AS min_create_time,
+					comment.create_time AS max_create_time
+				FROM 
+					{$this->_tableComment} AS comment 
+				WHERE 
+					{$where} AND comment.quiz_id = %d"
+				, $refIdUserId, $quizId)
+			, ARRAY_A);
+		$r = array();
+		foreach ($results as $row) {
+			$r[] = new WpProQuiz_Model_Comment($row);
+		}
+
+		return $r;
+	}
+
+}
