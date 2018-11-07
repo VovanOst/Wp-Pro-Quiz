9249afc1d33fb048cf053a6d5c8d27a4b27a7d9b
Vladimir <vovan.ost@mail.ru>
2018-11-07 16:43:18 +0300

    add fancy code for close fancy modal windows

--- js/wpProQuiz_front.js
+++ js/wpProQuiz_front.js
@@ -1433,6 +1433,7 @@ wpProQuizReady(function () {
                 globalElements.results.show();
 
                 plugin.methode.scrollTo(globalElements.results);
+                parent.jQuery.fancybox.close();
             },
 
             setCategoryOverview: function () {
